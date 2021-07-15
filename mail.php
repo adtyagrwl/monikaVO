63ee86491aa6c0d5a047af57bdbc5b80ecc94194
adtyagrwl <78271189+adtyagrwl@users.noreply.github.com>
2021-07-15 14:02:43 +0400

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,17 +1,14 @@
 <?php
-//get data from form  
 
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
 $to = "monikavo@gmail.com";
 $subject = "Query from Website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: monikavo@gmail.com" . "\r\n" .
-";
-if($email!=NULL){
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . 
+$message;
+    $headers = "From: monikavo@gmail.com" . "\r\n" .;
+    if($email!=NULL){
     mail($to,$subject,$txt,$headers);
-}
-//redirect
-header("Location:thankyou.html");
+    }
 ?>
